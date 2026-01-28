40dde2a83557d5f2d34e2b79a9dcad81adfcc773
pixdiego74 <st4059@utr.edu.mx>
2026-01-28 12:32:14 -0600

    Ejercicio aun Noob

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,6 +4,8 @@ import { Button, StyleSheet, Text, View } from "react-native";
 export default function RootLayout() {
   const [mood, setMood] = useState('Neutral 😐');
 
+  const [control, setControl] = useState(20);
+
   return (
     <View style={styles.container} >
       <Text style={styles.title} >Hoy me siento: {mood}</Text>
@@ -11,6 +13,12 @@ export default function RootLayout() {
         <Button title="Feliz 😊" onPress={() => setMood("Feliz 😊")}/>
         <Button title="Cansado 🥱" onPress={() => setMood("Cansado 🥱")}/>
         <Button title="Productivo 🫡" onPress={() => setMood("Productivo 🫡")}/>
+      </View>
+
+      <Text style={{fontSize: control}}>Controlo mi tamaño</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Mas" onPress={() => setControl(control +1)}/>
+        <Button title="Menos" onPress={() => setControl(control -1)}/> 
       </View>
     </View>
   );
@@ -24,6 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
+    fontSize: 24,
+    marginBottom: 20
+  },
+    titleControl: {
     fontSize: 24,
     marginBottom: 20
   },
